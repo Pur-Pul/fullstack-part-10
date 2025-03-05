ca024de794ad1a3ebee1f35a299e6a7722c9b177
PurPul <axbergsamuel@gmail.com>
2025-03-05 15:33:57 +0200

    Exercise 10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,6 +3,9 @@ import useRepositories from '../hooks/useRepositories';
 import RepositoryItem from './RepositoryItem';
 import {Picker} from '@react-native-picker/picker';
 import { useState } from 'react';
+import React from 'react';
+import TextInput from './TextInput';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
@@ -10,12 +13,63 @@ const styles = StyleSheet.create({
   },
 });
 
-const ItemSeparator = () => <View style={styles.separator} />;
+const RepositoryListHeader = ({ sort, setSort, search, setSearch}) => {
+  
+  return (
+    <View>
+      <TextInput
+        placeholder="Search..."
+        value={ search }
+        onChangeText={ text => setSearch(text) }
+      />
+      <Picker selectedValue={sort} onValueChange={(itemValue, itemIndex) => setSort(itemValue)}>
+        <Picker.Item label='Latest repositories' value='CREATED_AT;DESC' />
+        <Picker.Item label='Oldest repositories' value='CREATED_AT;ASC' />
+        <Picker.Item label='Highest rated repositories' value='RATING_AVERAGE;DESC' />
+        <Picker.Item label='Lowest rated repositories' value='RATING_AVERAGE;ASC' />
+      </Picker>
+    </View>
+  );
+}
 
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    // this.props contains the component's props
+    const props = this.props;
+
+    // ...
+
+    return (
+      <RepositoryListHeader
+        sort={ props.sort }
+        setSort={ props.setSort }
+        search={ props.search }
+        setSearch={ props.setSearch }
+      // ...
+      />
+    );
+  };
+
+  ItemSeparator = () => <View style={styles.separator} />;
+
+  render() {
+    
+    const repositoryNodes = this.props.repositories ? this.props.repositories.edges.map(edge => edge.node) : [];
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={this.ItemSeparator}
+        renderItem={({item}) => <RepositoryItem item={item}/>}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={this.renderHeader}
+      />
+    );
+  }
+}
+
+/*
 export const RepositoryListContainer = ({ repositories, sort='CREATED_AT;DESC', setSort=()=>{} }) => {
   const repositoryNodes = repositories ? repositories.edges.map(edge => edge.node) : [];
-  
-
   return (
     <FlatList
       data={repositoryNodes}
@@ -33,13 +87,14 @@ export const RepositoryListContainer = ({ repositories, sort='CREATED_AT;DESC', 
     />
   );
 }
-
+*/
 const RepositoryList = () => {
   const [sort, setSort] = useState('CREATED_AT;DESC')
+  const [search, setSearch] = useState('')
+  const [searchKeyword] = useDebounce(search, 500)
   const [orderBy, orderDirection] = sort.split(';')
-  const { repositories } = useRepositories({ orderBy, orderDirection });
-  
-  return <RepositoryListContainer repositories={ repositories } sort={sort} setSort={setSort}/>;
+  const { repositories } = useRepositories({ orderBy, orderDirection, searchKeyword });
+  return <RepositoryListContainer repositories={ repositories } sort={sort} setSort={setSort} search={search} setSearch={setSearch}/>;
   
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,8 +2,8 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 
-const useRepositories = ({ orderBy, orderDirection }) => {
-    const { data, loading, refetch } = useQuery(GET_REPOSITORIES, { variables: { orderBy, orderDirection }, fetchPolicy: 'cache-and-network' });
+const useRepositories = ({ orderBy, orderDirection, searchKeyword }) => {
+    const { data, loading, refetch } = useQuery(GET_REPOSITORIES, { variables: { orderBy, orderDirection, searchKeyword }, fetchPolicy: 'cache-and-network' });
 
     if (loading) {
         return { data, loading, refetch }
